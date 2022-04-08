574957073db53d6553374778e22f1c736686af61
Subham Gupta <99577900+0subham1@users.noreply.github.com>
2022-04-08 15:36:55 +0530

    1w

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react'
-import { useEffect } from 'react'
+import React from "react";
+import { useEffect } from "react";
 // ea3f4e91
-import './App.css'
-import SearchIcon from './search.svg'
+import "./App.css";
+import SearchIcon from "./search.svg";
 
-const API_URL= "http://www.omdbapi.com?apikey=ea3f4e91"
+const API_URL = "http://www.omdbapi.com?apikey=ea3f4e91";
 const App = () => {
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
 
-    const searchMovie = async(title)=>{
-        const response =await fetch(`${API_URL}&s=${title}`)
-        const data =await response.json()
-        console.log(data.Search)
-    }
+  useEffect(() => {
+    searchMovie("batman");
+  }, []);
 
-useEffect(()=>{
-searchMovie('batman')
-},[])
   return (
-    <div>hello world</div>
-  )
-}
+    <div className="app">
+      <h1>Movie Land</h1>
 
-export default App
+      <input placeholder="search for movies"
+      value={"superman"}
+      onChange={()=>{}}/>
+
+       <button onClick={()=>{}}>Search</button>
+    </div>
+  );
+};
+
+export default App;
