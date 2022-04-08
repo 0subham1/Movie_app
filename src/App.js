01d43bcf1fb1be5a996edbafdc53e5cdc505777a
Subham Gupta <99577900+0subham1@users.noreply.github.com>
2022-04-08 13:11:30 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useEffect } from 'react'
+// ea3f4e91
 
-function App() {
+const API_URL= "http://www.omdbapi.com?apikey=ea3f4e91"
+const App = () => {
+
+useEffect(()=>{
+    
+})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
