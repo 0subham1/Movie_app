a21d01892a64962838141e8c96176962905ea887
Subham Gupta <99577900+0subham1@users.noreply.github.com>
2022-04-08 13:43:06 +0530

    1q

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import { useEffect } from 'react'
 // ea3f4e91
+import './App.css'
+import SearchIcon from './search.svg'
 
 const API_URL= "http://www.omdbapi.com?apikey=ea3f4e91"
 const App = () => {
 
+    const searchMovie = async(title)=>{
+        const response =await fetch(`${API_URL}&s=${title}`)
+        const data =await response.json()
+        console.log(data.Search)
+    }
+
 useEffect(()=>{
-    
-})
+searchMovie('batman')
+},[])
   return (
-    <div>App</div>
+    <div>hello world</div>
   )
 }
 
